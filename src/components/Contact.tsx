d42708dde60909945e315c8044c3dad6fff234c3
VladT7 <vladt13@gmail.com>
2023-04-25 13:43:45 -0600

    update contact info

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,24 @@
+import { Container, Form, Button } from "react-bootstrap";
+
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <Container>
+      <h2>Contact Me</h2>
+      <Form className="mt-5">
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group controlId="formBasicMessage">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows={5} placeholder="Enter message" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
 }
 
 export default Contact;
