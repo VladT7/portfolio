5050a710298d9d1d94b41567e7219e2218a92f06
VladT7 <vladt13@gmail.com>
2023-04-28 17:20:06 -0600

    fix form submission

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -13,19 +13,32 @@ function Contact() {
           name="contact"
           method="POST"
           data-netlify="true"
-          data-netlify-honeypot="bot-field"
+          // data-netlify-honeypot="bot-field"
         >
           <Form.Group controlId="formBasicName">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="name" placeholder="Enter your name" />
+            <Form.Control
+              type="name"
+              placeholder="Enter your name"
+              name="name"
+            />
           </Form.Group>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter your email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+            />
           </Form.Group>
           <Form.Group controlId="formBasicMessage">
             <Form.Label>Message</Form.Label>
-            <Form.Control as="textarea" rows={5} placeholder="Enter message" />
+            <Form.Control
+              as="textarea"
+              rows={5}
+              placeholder="Enter message"
+              name="message"
+            />
           </Form.Group>
           <Button variant="primary" type="submit" style={{ marginTop: 20 }}>
             Submit
