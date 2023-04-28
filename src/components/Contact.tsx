ea3bfb6936e0168407f1d6c90140ee020f42174f
VladT7 <vladt13@gmail.com>
2023-04-28 17:24:31 -0600

    Revert "Merge pull request #6 from VladT7/dev"
    This reverts commit 53df115b175dcebf6fb25a24a3adc4eed62cd862, reversing
    changes made to 75645e85edae06041c0e43b38798c49694cbc16b.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,27 +1,14 @@
 import { Container, Form, Button } from "react-bootstrap";
 
-// interface FormProps extends HTMLAttributes<HTMLFormElement> {
-//   netlify?: boolean;
-// }
 function Contact() {
   return (
     <div id="contact">
       <Container style={{ marginBottom: 20 }}>
         <h2>Contact Me</h2>
-        <Form
-          className="mt-5"
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <Form.Group controlId="formBasicName">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="name" placeholder="Enter your name" />
-          </Form.Group>
+        <Form className="mt-5">
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter your email" />
+            <Form.Control type="email" placeholder="Enter email" />
           </Form.Group>
           <Form.Group controlId="formBasicMessage">
             <Form.Label>Message</Form.Label>
