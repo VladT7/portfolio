914ef021955ef409294f1655e921c2d78f57953f
VladT7 <vladt13@gmail.com>
2023-05-01 23:08:08 -0600

    form control

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,10 +5,15 @@ function Contact() {
     <div id="contact">
       <Container style={{ marginBottom: 20 }}>
         <h2>Contact Me</h2>
-        <Form className="mt-5">
+        <Form className="mt-5" data-netlify="true" name="contact" method="POST">
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              name="email"
+              netlify-honeypot="bot-field"
+            />
           </Form.Group>
           <Form.Group controlId="formBasicMessage">
             <Form.Label>Message</Form.Label>
@@ -17,6 +22,11 @@ function Contact() {
               rows={5}
               placeholder="Enter message"
               name="message"
+            />
+            <Form.Control
+              type="text"
+              name="bot-field"
+              style={{ display: "none" }}
             />
           </Form.Group>
           <Button variant="primary" type="submit" style={{ marginTop: 20 }}>
