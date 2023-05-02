615bbc60fa021fa5829d40c007e07734493aede8
VladT7 <vladt13@gmail.com>
2023-05-02 15:26:37 -0600

    fix icokns

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,9 @@
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 
 function Contact() {
   return (
     <div id="contact">
-      <Container style={{ marginBottom: 20 }}>
+      <Container>
         <h2>Contact Me</h2>
         <p>
           Interested in working together? Fill out the form below or shoot me an
@@ -11,23 +11,16 @@ function Contact() {
         </p>
         <form name="contact" method="post">
           <input type="hidden" name="form-name" value="contact" />
+          <label>Your Name:</label>
+          <input className="form-control" type="text" name="name" />
+          <label>Your Email:</label>
+          <input className="form-control" type="email" name="email" />
+          <label>Message:</label>
+          <textarea className="form-control" name="message"></textarea>
           <p>
-            <label>
-              Your Name: <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message"></textarea>
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
+            <Button type="submit" style={{ marginTop: 10 }}>
+              Send
+            </Button>
           </p>
         </form>
       </Container>
