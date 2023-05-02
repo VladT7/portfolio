48431bbb0f78159a1f182e8a5167625ba74be9ed
VladT7 <vladt13@gmail.com>
2023-05-02 12:14:35 -0600

    update form, add scrolling buttons

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,29 +6,18 @@ function Contact() {
       <Container style={{ marginBottom: 20 }}>
         <h2>Contact Me</h2>
         <Form className="mt-5" data-netlify="true" name="contact" method="POST">
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              name="email"
-              netlify-honeypot="bot-field"
-            />
-          </Form.Group>
-          <Form.Group controlId="formBasicMessage">
-            <Form.Label>Message</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={5}
-              placeholder="Enter message"
-              name="message"
-            />
-            <Form.Control
-              type="text"
-              name="bot-field"
-              style={{ display: "none" }}
-            />
-          </Form.Group>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            netlify-honeypot="bot-field"
+          />
+          <Form.Control
+            as="textarea"
+            rows={5}
+            placeholder="Enter message"
+            name="message"
+          />
           <Button variant="primary" type="submit" style={{ marginTop: 20 }}>
             Submit
           </Button>
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,6 +1,6 @@
 import { Row, Col, Image, Button } from "react-bootstrap";
 import ResumeButton from "./ResumeButton";
-// import { Link } from "react-scroll";
+import { Link } from "react-scroll";
 function LandingPage() {
   /**
    * a cool effect I thought of for this page - a tilt shift effect on the image that scrolls through the image as the user scrolls through the page
@@ -15,11 +15,12 @@ function LandingPage() {
           As a full-stack software engineer, I turn your ideas into reality with
           precision and passion. Help your projects come to life - let's chat!
         </p>
-        <ResumeButton></ResumeButton> {/* <Link to="contact" smooth={true}> */}
-        <Button variant="secondary" size="lg" style={{ marginBottom: 20 }}>
-          Get in touch
-        </Button>
-        {/* </Link> */}
+        <ResumeButton></ResumeButton>
+        <Link to="contact" smooth={true} duration={10}>
+          <Button variant="secondary" size="lg" style={{ marginBottom: 20 }}>
+            Get in touch
+          </Button>
+        </Link>
       </Col>
     </Row>
   );
